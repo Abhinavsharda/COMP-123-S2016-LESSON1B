365b4fdeae15f8eaf82e0330add037c8535a76b8
Abhinavsharda <abhinavsharda33@gmail.com>
2016-05-13 10:44:11 -0400

    Initial commit

---
+++ COMP123_S2016_lESSON1B/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Aurthor: Abhinav Sharda
+ * DAte: 13 May,2016
+ * Student ID: 300868625
+ * 
+ * Discruption: This program demonstrate Github control and vision contro
+ *best practicse
+ * /
+
+
+namespace COMP123_S2016_lESSON1B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
